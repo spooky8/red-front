53e8e80bec7a8335c9c103711393fedc2abbb65e
spooky <zxoocy@gmail.com>
2025-05-16 00:33:45 +0700

    configure prettier + tsconfig + next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  poweredByHeader: false,
+  env: {
+    APP_URL: process.env.APP_URL,
+    API_URL: process.env.API_URL,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.API_URL}/:path*`,
+      },
+      {
+        source: '/uploads/:path*',
+        destination: 'http://localhost:4200/uploads/:path*',
+      },
+    ]
+  },
+}
 
-export default nextConfig;
+export default nextConfig
